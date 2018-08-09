2561643d2f15cd002b3c89d06fd64ec3b00dc494
Jeff Evans <Jeff.L.Evans@conocophillips.com>
2018-08-09 09:57:16 -0500

    add css for chat page

--- server/server.js
+++ server/server.js
@@ -21,11 +21,11 @@ io.on('connection', (socket) => {
     socket.on('createMessage', (message, callback) => {
         console.log(message);
         io.emit('newMessage', generateMessage(message.from, message.text));
-        callback('This is from the server.');
+        callback();
     });
 
     socket.on('createLocationMessage', (coords) => {
-        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
     });
 
     socket.on('disconnect', () => {
