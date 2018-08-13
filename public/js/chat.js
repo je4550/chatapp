64ac8f21f3c634bdc344897b607ddfe64dbe7816
Jeff Evans <Jeff.L.Evans@conocophillips.com>
2018-08-13 10:19:27 -0500

    add users class and update user list

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,8 +2,8 @@ var socket = io();
 
 function scrollToBottom() {
     // Selectors
-    var messages = jQuery("#messages");
-    var newMessage = messages.children('li:last-child');
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child')
     // Heights
     var clientHeight = messages.prop('clientHeight');
     var scrollTop = messages.prop('scrollTop');
@@ -11,22 +11,41 @@ function scrollToBottom() {
     var newMessageHeight = newMessage.innerHeight();
     var lastMessageHeight = newMessage.prev().innerHeight();
 
-    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
         messages.scrollTop(scrollHeight);
     }
 }
 
 socket.on('connect', function () {
-    console.log('Connected to server');
+    var params = jQuery.deparam(window.location.search);
+
+    socket.emit('join', params, function (err) {
+        if (err) {
+            alert(err);
+            window.location.href = '/';
+        } else {
+            console.log('No error');
+        }
+    });
 });
 
-socket.on('disconnected', function () {
+socket.on('disconnect', function () {
     console.log('Disconnected from server');
 });
 
+socket.on('updateUserList', function (users) {
+    var ol = jQuery('<ol></ol>');
+
+    users.forEach(function (user) {
+        ol.append(jQuery('<li></li>').text(user));
+    });
+
+    jQuery('#users').html(ol);
+});
+
 socket.on('newMessage', function (message) {
-    var formattedTime = moment(message.createdAt).format("h:mm a");
-    var template = jQuery("#message-template").html();
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#message-template').html();
     var html = Mustache.render(template, {
         text: message.text,
         from: message.from,
@@ -38,16 +57,16 @@ socket.on('newMessage', function (message) {
 });
 
 socket.on('newLocationMessage', function (message) {
-    var formattedTime = moment(message.createdAt).format("h:mm a");
-    var template = jQuery("#location-message-template").html();
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#location-message-template').html();
     var html = Mustache.render(template, {
-        url: message.url,
         from: message.from,
+        url: message.url,
         createdAt: formattedTime
     });
 
     jQuery('#messages').append(html);
-    scrollToBottom(); 
+    scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
@@ -59,26 +78,26 @@ jQuery('#message-form').on('submit', function (e) {
         from: 'User',
         text: messageTextbox.val()
     }, function () {
-        messageTextbox.val('');
+        messageTextbox.val('')
     });
 });
 
 var locationButton = jQuery('#send-location');
 locationButton.on('click', function () {
     if (!navigator.geolocation) {
-        return alert('Geolocation Not Support By Your Browser');
+        return alert('Geolocation not supported by your browser.');
     }
 
-    locationButton.attr('disabled', 'disabled').text('Sending Location ...');
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
 
     navigator.geolocation.getCurrentPosition(function (position) {
-        locationButton.removeAttr('disabled').text('Send Location');
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
-        locationButton.removeAttr('disabled').text('Send Location');
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     });
 });
